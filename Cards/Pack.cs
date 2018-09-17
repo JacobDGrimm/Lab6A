0a9db1870e598c82af92c64b8d11b61f698816a4
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-17 17:18:58 -0400

    adds some code from the book

---
+++ Cards/Pack.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Cards
+{	
+	class Pack
+	{
+        public const int NumSuits = 4;
+        public const int CardsPerSuit = 13;
+        private PlayingCard[,] cardPack;
+        private Random randomCardSelector = new Random();
+
+        public Pack()
+        {
+            this.cardPack = new PlayingCard[NumSuits, CardsPerSuit];
+            for(Suit suit = Suit.Clubs; suit <= Suit.Spades; suit++)
+            {
+                for (Value value = Value.Two; value <= Value.Ace; value ++)
+                {
+                    this,cardPack[(int)suit, (int)value] = new PlayingCard(suit, value);
+                }
+            }
+        }
+
+        public PlayingCard DealCardFromPack()
+        {
+            Suit suit = (Suit)randomCardSelector.Next(NumSuits);
+            while (this.IsSuitEmpty(suit))
+            {
+                suit = (Suit)randomCardSelector.Next(NumSuits);
+            }
+        }
+
+        private bool IsSuitEmpty(Suit suit)
+        {
+            // TODO: return true if there are no more cards available in this suit
+            throw new NotImplementedException("IsSuitEmpty - TBD");
+        }
+
+        private bool IsCardAlreadyDealt(Suit suit, Value value)
+        {
+            // TODO: return true if this card has already been dealt   
+            throw new NotImplementedException("IsCardAlreadyDealt - TBD");
+        }
+    }
+}
